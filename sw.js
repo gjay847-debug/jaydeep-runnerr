4f0f4d37f5a151c698e327c59846838b46f453a3
gjay847-debug <gjay847@gmail.com>
2025-11-20 01:10:49 +0530

    Refactor service worker caching logic

--- sw.js
+++ sw.js
@@ -1,22 +1,23 @@
-const CACHE_NAME = 'jaydeep-runner-v1';
-const FILES_TO_CACHE = [
-  '/jaydeep-runnerr/',
-  '/jaydeep-runnerr/index.html',
-  '/jaydeep-runnerr/style.css',
-  '/jaydeep-runnerr/script.js',
-  '/jaydeep-runnerr/icon-192.png',
-  '/jaydeep-runnerr/icon-512.png'
-];
-
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("jaydeep-runner-cache").then((cache) => {
+            return cache.addAll([
+                "./",
+                "./index.html",
+                "./style.css",
+                "./script.js",
+                "./manifest.json",
+                "./icon-192.png",
+                "./icon-512.png"
+            ]);
+        })
+    );
 });
 
-self.addEventListener('activate', (evt) => {
-  evt.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (evt) => {
-  evt.respondWith(caches.match(evt.request).then(res => res || fetch(evt.request)));
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
